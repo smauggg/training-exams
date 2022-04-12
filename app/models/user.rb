5b2422c3765e942bcd7b49a15ed6d8292c5ef4a6
minhpham <minhpham161895@gmail.com>
2022-04-12 17:24:20 +0700

    password reset 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :user_exams, dependent: :destroy
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   validates :name, presence: true,
                    length: { maximum: Settings.model.user.name_length_max }
   validates :email, format: { with: Settings.email_regex },
@@ -9,16 +9,16 @@ class User < ApplicationRecord
                     presence: true
   has_secure_password
   validates :password, length: { minimum: Settings.model.user.password_length_min },
-                        presence: true
+                       presence: true
 
-  enum role: {user: 0, admin: 1}
+  enum role: { user: 0, admin: 1 }
 
   def remember
     self.remember_token = User.new_token
     update_column :remember_digest, User.digest(remember_token)
   end
 
-  def authenticated? attribute, token
+  def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return unless digest
 
@@ -29,19 +29,33 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
   class << self
-    def digest string
+    def digest(string)
       cost = if ActiveModel::SecurePassword.min_cost
-               BCrypt::Engine::MIN_COST
-             else
-               BCrypt::Engine.cost
-             end
+          BCrypt::Engine::MIN_COST
+        else
+          BCrypt::Engine.cost
+        end
       BCrypt::Password.create(string, cost: cost)
     end
 
     def new_token
       SecureRandom.urlsafe_base64
     end
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
     resources :users, only: %i(create new show)
+    resources :password_resets
   end
 end
