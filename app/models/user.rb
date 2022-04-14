d7c99c69c5d6621e9776c75a4a67c13bf3b9770a
minhpham <minhpham161895@gmail.com>
2022-04-14 14:23:27 +0700

    password_reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :user_exams, dependent: :destroy
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :reset_token
   validates :name, presence: true,
                    length: { maximum: Settings.model.user.name_length_max }
   validates :email, format: { with: Settings.email_regex },
@@ -9,33 +9,24 @@ class User < ApplicationRecord
                     presence: true
   has_secure_password
   validates :password, length: { minimum: Settings.model.user.password_length_min },
-                        presence: true
+                       presence: true
 
-  enum role: {user: 0, admin: 1}
+  enum role: { user: 0, admin: 1 }
 
-  def remember
-    self.remember_token = User.new_token
-    update_column :remember_digest, User.digest(remember_token)
-  end
-
-  def authenticated? attribute, token
+  def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return unless digest
 
     BCrypt::Password.new(digest).is_password?(token)
   end
 
-  def forget
-    update_attribute(:remember_digest, nil)
-  end
-
   class << self
-    def digest string
+    def digest(string)
       cost = if ActiveModel::SecurePassword.min_cost
-               BCrypt::Engine::MIN_COST
-             else
-               BCrypt::Engine.cost
-             end
+          BCrypt::Engine::MIN_COST
+        else
+          BCrypt::Engine.cost
+        end
       BCrypt::Password.create(string, cost: cost)
     end
 
@@ -44,10 +35,31 @@ class User < ApplicationRecord
     end
   end
 
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
   private
 
   def downcase_email
     self.email = email.downcase
   end
-  
 end
